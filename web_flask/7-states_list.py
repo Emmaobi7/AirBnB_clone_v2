681f96c568e90cd4a00661d4079bd92ac8186943
Emmaobi7 <emma46574@gmail.com>
2023-09-25 10:53:39 -0700

    added files

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""display my data"""
+
+from flask import Flask, render_template
+from models import *
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """
+    display list of states
+    in database in a page
+    """
+    states = sorted(list(storage.all('State').values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
+
+@app.teardown_appcontext
+def clean_up_db(exception):
+    """
+    closes mysqlalcmy session
+    uses app.teardown_context
+    """
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
